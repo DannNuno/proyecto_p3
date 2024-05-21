086b971fbba18feea1cc7b4d8997756ecd44b0ea
Daniela Nuño <13044@portalcedi.edu.mx>
2024-05-21 12:36:12 -0600

    pollito

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 using namespace sf;
+using namespace std;
 int main()
 {
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
-    CircleShape shape(100.f);
-    shape.setFillColor(Color::Green);
+    RenderWindow window(VideoMode(1920, 1080), "SFML works!");
+    window.setFramerateLimit(60);
+    Texture polloTexture;
 
+    if(!polloTexture.loadFromFile("assets/pollito.png"))
+    {
+        cout << "Error al cargar imagen" << endl;
+    }
+    polloTexture.setRepeated(true);
+
+    Sprite pollo;
+    pollo.setTexture(polloTexture);
+    pollo.setScale(10,10);
+    int opacidad = 0;
+    
     while (window.isOpen())
     {
         Event event;
@@ -15,9 +28,11 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(Color(51,51,51));
+        window.draw(pollo);
+        pollo.setColor(Color(255,255,255, opacidad % 256));
         window.display();
+        opacidad++;
     }
 
     return 0;
